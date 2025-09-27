2f3bb0bc256876b569be723655bac328867345ac
Ricardo Araujo <ricardoasf@id.uff.br>
2025-09-27 13:52:11 -0300

    feat: add name components

--- src/game/scenes/PlayerSelection.js
+++ src/game/scenes/PlayerSelection.js
@@ -1,17 +1,31 @@
 import { Scene } from "phaser"
 import { EventBus } from "../EventBus";
 import { GameConfig } from "../config/gameConfig";
+import { CARD_LAYOUTS, CARD_WIDTH, NAME_COMPONENT } from "../config/playerSelectionCardData";
 
 export class PlayerSelection extends Scene {
     constructor () {
         super('PlayerSelection');
     }
 
+    createNameComponent(player) {
+        const componentRectangle = this.add
+            .rectangle(
+                player.x, NAME_COMPONENT.y, 
+                CARD_WIDTH, NAME_COMPONENT.height, 
+                player.color, 1
+            )
+            .setOrigin(0)
+            .setVisible(true);   
+        
+        return componentRectangle;
+    }
+
     create () {
         this.add.image(0, 0, 'main-background').setOrigin(0);
 
         const buttonText = this.add.text(
-            (GameConfig.width / 2), (GameConfig.height / 2), 
+            (GameConfig.width / 2), (GameConfig.height - 20), 
             'INICIAR', 
             {
                 fontFamily: 'JetBrainsMono',
@@ -26,6 +40,12 @@ export class PlayerSelection extends Scene {
         buttonText.on('pointerdown', () => {
             this.changeScene();
         });
+
+        this.createNameComponent(CARD_LAYOUTS.player1);
+        this.createNameComponent(CARD_LAYOUTS.player2);
+        this.createNameComponent(CARD_LAYOUTS.player3);
+        this.createNameComponent(CARD_LAYOUTS.player4);
+        this.createNameComponent(CARD_LAYOUTS.player5);
 
         EventBus.emit('current-scene-ready', this);
     }
