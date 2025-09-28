8b74a1ffd0b6fc92276bc1183a07bafb381c4ab3
Ricardo Araujo <ricardoasf@id.uff.br>
2025-09-28 13:59:47 -0300

    refactor(colors): delete quotation marks from fields identifiers

--- src/game/config/colors.js
+++ src/game/config/colors.js
@@ -1,10 +1,10 @@
 export const COLOR = {
-    'brand': 0xdbee42,
-    'primary': 0x1c1c1c,
-    'secondary': 0x141414,
-    'pink': 0xff61c6,
-    'blue': 0x5cecff,
-    'orange': 0xff9900,
-    'purple': 0x7d53ff,
-    'black': 0x000000
+    brand: 0xdbee42,
+    primary: 0x1c1c1c,
+    secondary: 0x141414,
+    pink: 0xff61c6,
+    blue: 0x5cecff,
+    orange: 0xff9900,
+    purple: 0x7d53ff,
+    black: 0x000000
 }
