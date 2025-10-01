30dcd772d311b278bd9eab9763492b8593879f02
Ricardo Araujo <ricardoasf@id.uff.br>
2025-09-30 22:20:54 -0300

    refactor: change player and ia types to human and bot

---
+++ src/game/config/playerTypes.js
@@ -0,0 +1,11 @@
+export const PLAYER_TYPES = {
+    HUMAN: 'human',
+    BOT: 'bot',
+    NONE: 'none'
+};
+
+export const PLAYER_TYPE_LABELS = {
+    [PLAYER_TYPES.HUMAN]: 'JOGADOR',
+    [PLAYER_TYPES.BOT]: 'BOT',
+    [PLAYER_TYPES.NONE]: 'NONE'
+};
