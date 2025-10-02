87c37cfc7ac76778957a246da5a742544d265a7c
gdac <gabrieldalcs@gmail.com>
2025-10-02 19:17:02 -0300

    feat(players): add territory card distribution to players randomly

--- src/game/managers/GameStateManager.js
+++ src/game/managers/GameStateManager.js
@@ -1,3 +1,4 @@
+import Player from '../gameObjects/Player';
 import Territory from '../gameObjects/Territory';
 
 
@@ -7,7 +8,13 @@ export default class GameStateManager extends Phaser.Events.EventEmitter {
         this.scene = scene;
         this.territories = {};
         this.continents = {};
+        this.players = []
         this.initializeMap();
+        // must be called with parameters from UI
+        this.initializePlayers();
+        console.log(this.players)
+        this.distributeTerritories();
+    
     }
 
     initializeMap() {
@@ -28,4 +35,41 @@ export default class GameStateManager extends Phaser.Events.EventEmitter {
             });
         });
     }
+
+    distributeTerritories(){
+        const territoriesIds = Object.values(this.territories).map(terrt => terrt.id);
+        
+        for (let i = territoriesIds.length - 1; i > 0; i--){
+            const j = Math.floor(Math.random() * (i + 1));
+            [territoriesIds[i], territoriesIds[j]] = [territoriesIds[j], territoriesIds[i]];
+        }
+
+        
+        territoriesIds.forEach(
+            (id, index) => {
+                const playerIndex = index % this.players.length;
+                const player = this.players[playerIndex];
+                const territory = this.territories[id];
+                player.addTerritory(territory);
+                territory.setOwner(player);
+            }
+        )
+    }
+    
+    initializePlayers(playerParams = []){
+        // TODO
+        // Mock implementation to test
+        const p1 = new Player("gdac", "#f0b069")
+        const p2 = new Player("rick", "#55c1db")
+        const p3 = new Player("pcb", "#d2cfca")
+        const p4 = new Player("igor", "#ff0088")
+        this.players.push(p1, p2, p3, p4)
+    }
+
+
+
+
+        
+
+        
 }
