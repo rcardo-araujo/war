3e07c17648a62342a3ac6176436888362597ec4f
gdac <gabrieldalcs@gmail.com>
2025-10-23 18:44:34 -0300

    refactor(objective): add object destructuring in Objective class

--- src/game/gameObjects/Objective.js
+++ src/game/gameObjects/Objective.js
@@ -1,5 +1,5 @@
 export default class Objective {
-    constructor(main, fallback, type, targetColor, description){
+    constructor({main, fallback, type, description}){
         this.type = type;
         this.target = target;
         this.main = main;
