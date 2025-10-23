f62e70c0cbebd00c974136f5da77700b977bc0b3
gdac <gabrieldalcs@gmail.com>
2025-10-23 14:44:25 -0300

    feat(logic): add players data from PlayerSelection in GameStateManager constructor

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -17,7 +17,7 @@ export class Game extends Scene
 
     create ()
     {
-        this.gameState = new GameStateManager(this);
+        this.gameState = new GameStateManager(this, this.playerSetup);
 
         this.add.image(0, 0, 'board-background')
             .setOrigin(0)
