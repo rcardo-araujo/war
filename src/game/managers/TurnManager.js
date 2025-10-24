87c7ef8752bfad4eb9de7b5bf621495b2ff3cc53
gdac <gabrieldalcs@gmail.com>
2025-10-24 17:53:42 -0300

    feat(turn): add TurnManager to control turn variables

---
+++ src/game/managers/TurnManager.js
@@ -0,0 +1,63 @@
+export const TURN_PHASES = Object.freeze({
+    REINFORCEMENT: "reinforcement",
+    ATTACK: "attack",
+    STRATEGIC: "strategic",
+    END: "END"
+})
+
+export default class TurnManager {
+    constructor(player = []){
+        this.players = this.players;
+        this.currentPlayerIndex = 0;
+        this.currentPhase = TURN_PHASES.REINFORCEMENT;
+        this.hasPerformedStrategicMove = false;
+    }
+
+    setPlayers(players = []){
+        this.players = players;
+        this.currentPlayerIndex;
+        this.resetTurnState();
+    }
+
+    resetTurnState(){
+        this.currentPhase = TURN_PHASES.REINFORCEMENT;
+        this.hasPerformedStrategicMove = false;
+    }
+
+    getCurrentPlayer(){
+        return this.players[this.currentPlayerIndex] || null;
+    }
+
+    getCurrentPhase(){
+        return this.currentPhase;
+    }
+
+    setPhase(phase){
+        if (!Object.values(TURN_PHASES).includes(phase)){
+            throw new Error(`Invalid turn phase: ${phase}`)
+        }
+
+        this.currentPhase = phase;
+        if (phase == TURN_PHASES.STRATEGIC){
+            this.hasPerformedStrategicMove = false;
+        }
+    }
+
+    markStrategicMovePerformed(){
+        this.hasPerformedStrategicMove = true;
+    }
+
+    hasStrategicMoveAvailable(){
+        return this.getCurrentPhase === TURN_PHASES.STRATEGIC && !this.hasPerformedStrategicMove;
+    }
+
+    endTurn(){
+        if (this.players.length === 0){
+            this.currentPlayerIndex = 0;
+            this.resetTurnState();
+            return;
+        }
+        this.currentPlayerIndex = (this.currentPlayerIndex + 1) % this.players.length;
+        this.resetTurnState();
+    }
+}
