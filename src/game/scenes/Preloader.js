d42f40ba0e94cc375faac7cc305f45ba9cb5ca2c
Ricardo Araujo <ricardoasf@id.uff.br>
2025-11-14 20:17:03 -0300

    feat: add target new assets and remove older ones

--- src/game/scenes/Preloader.js
+++ src/game/scenes/Preloader.js
@@ -43,11 +43,8 @@ export class Preloader extends Scene
         this.load.image('board-background', 'assets/images/maps/board-background.png');
         this.load.image('main-background', 'assets/images/main-background.png');
 
-        this.load.image('target-brand', 'assets/images/ui/target-brand.png');
-        this.load.image('target-blue', 'assets/images/ui/target-blue.png');
-        this.load.image('target-orange', 'assets/images/ui/target-orange.png');
-        this.load.image('target-purple', 'assets/images/ui/target-purple.png');
-        this.load.image('target-pink', 'assets/images/ui/target-pink.png');
+        this.load.image('target-aim', 'assets/images/ui/target-aim.png');
+        this.load.image('target-elipse', 'assets/images/ui/target-elipse.png');
         this.load.image('objective-card', 'assets/images/ui/objective-card.png');
 
         this.load.audio('background-music', 'assets/audio/background.mp3');
