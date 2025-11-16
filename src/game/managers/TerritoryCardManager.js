7e88fa437168046a6a892f071544a4b84b02c334
Wallace Pimentel <wallace.r.p.jr@gmail.com>
2025-11-16 19:17:47 -0300

    feat: adding checking of trade eligibility and trade bonus calculation

--- src/game/managers/TerritoryCardManager.js
+++ src/game/managers/TerritoryCardManager.js
@@ -4,6 +4,9 @@ export default class TerritoryCardManager {
     constructor (mapData) {
         this.territoryCards = {};
         this.initializeTerritoryCards(mapData);
+        
+        this.usedTerritoryCards = new Set();
+        this.currentTrade = 1;
     }
 
     initializeTerritoryCards (mapData) {
@@ -15,6 +18,65 @@ export default class TerritoryCardManager {
     changePlayerTerritoryCardOwnership(territoryCardId, newOwner) {
         const territoryCard = this.getTerritoryCard(territoryCardId);
         territoryCard.setOwner(newOwner);
+        this.usedTerritoryCards.add(territoryCardId);
+    }
+
+    checkTradeEligibility(player) {
+        const playerOwnedCards = Object.values(this.territoryCards).filter(card => card.owner === player);
+        if (playerOwnedCards.length > 2) {
+            if (2 < playerOwnedCards.length < 5) {
+                circle_count = 0;
+                square_count = 0;
+                triangle_count = 0;
+
+                for (let card of playerOwnedCards) {
+                    if (card.type === 'circle') {
+                        circle_count += 1;
+                    } else if (card.type === 'square') {
+                        square_count += 1;
+                    } else if (card.type === 'triangle') {
+                        triangle_count += 1;
+                    }
+                }
+
+                if (circle_count === 3 || square_count === 3 || triangle_count === 3 || 
+                    (circle_count >= 1 && square_count >= 1 && triangle_count >= 1)) {
+                    return true;
+                }
+                return false;
+            }
+        }
+        else {
+            return true;
+        }
+    }   
+    
+
+    calculateTradeBonus(player, tradedCardIds) {
+        const baseBonus = 4;
+        
+        switch (this.currentTrade) {
+            case 1:
+                this.currentTrade += 1;
+                return baseBonus;
+            case 2:
+                this.currentTrade += 1;
+                return baseBonus + 2;
+            case 3: 
+                this.currentTrade += 1;
+                return baseBonus + 4;
+            case 4:
+                this.currentTrade += 1;
+                return baseBonus + 6;
+            case 5:
+                this.currentTrade += 1;
+                return baseBonus + 8;
+            case 6:
+                this.currentTrade += 1;
+                return baseBonus + 11;
+            default:
+                return baseBonus + 11 + (this.currentTrade - 6) * 5;
+        }
     }
 
 }
