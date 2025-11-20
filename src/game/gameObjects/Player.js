e44c9ec56a9379c57b44d3714b8df73f15f89870
gdac <gabrieldalcs@gmail.com>
2025-11-20 20:50:47 -0300

    add type check in Player class

--- src/game/gameObjects/Player.js
+++ src/game/gameObjects/Player.js
@@ -1,5 +1,5 @@
 export default class Player {
-    constructor(name, color, colorKey = null) {
+    constructor(name, color, colorKey = null, type = 'human') {
         this.name = name;
         this.color = color;
         this.colorKey = colorKey;
@@ -13,6 +13,7 @@ export default class Player {
         this.availableTroopsAsia = 0;
         this.availableTroopsAfrica = 0;
         this.availableTroopsOceania = 0;
+        this.type = type
     }
 
     getColor() {
